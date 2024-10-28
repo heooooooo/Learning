87189a3f9c37e94d4fb6516f5511b386ea7659ee
Hoieusac <24022789@vnu.edu.vn>
2024-10-28 09:42:05 +0700

    first commit

---
+++ new.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+    cout<<"Hello, world"<<endl;
+}
