1c240ef823845574e34f32d9235ad82357b3147d
Hoieusac <24022789@vnu.edu.vn>
2024-10-29 23:32:34 +0700

    Added my project

--- new.cpp
+++ new.cpp
@@ -1,17 +1,64 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
-    cout<<"Hello, world"<<endl;
+int main(){
+    string date;
+    cin >> date;
+    
+    string d = "", m = "", y = "";
+    d += date[0] + date[1];
+    m += date[3] + date[4];
+    y += date[6] + date[7] + date[8] + date[9];
+    cout << d;
+
+    int day = stoi(d);
+    int month = stoi(m);
+    int year = stoi(y);
+    
+    if (year >= 1 && year <= 2024){
+        if (month >= 1 && month <= 12){
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+                if (day >= 1 && day <= 31){
+                    cout << "yes";
+                }
+                else{
+                    cout << "no";
+                }
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11){
+                if (day >= 1 and day <= 30){
+                    cout << "yes";
+                }
+                else{
+                    cout << "no";
+                }
+            }
+            else{
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+                    if (day >= 1 && day <= 29){
+                        cout << "yes";
+                    }
+                    else{
+                        cout << "no";
+                    }
+                }
+                else{
+                    if (day >= 1 && day <= 28){
+                        cout << "yes";
+                    }
+                    else{
+                        cout << "no";
+                    }
+                }
+            }
+        }
+        else{
+            cout << "no";
+        }
+    }
+    else{
+        cout << "no";
+    }
 }
